9a6fb8c1fbd7987ceb0225d4f0a97ad864714a57
Aleksandra Odzierzynska <ola.odzierzynska@gmail.com>
2025-01-17 12:40:23 +0100

    first commit

---
+++ src/main/java/com/example/codewarspractice/CodewarspracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codewarspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewarspracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewarspracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codewarspractice/CodewarspracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codewarspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewarspracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
